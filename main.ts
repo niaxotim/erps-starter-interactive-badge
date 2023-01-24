c05fffa7e8158caccf3b519b0b593e6701b40887
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-24 10:55:03 +0000

    blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
